17a353afa58b81d60bfe56992be5ca8db50790c8
jimezapolski <jimezapolski@gmail.com>
2024-03-27 14:01:54 -0300

    start

---
+++ mostrarTareas.ts
@@ -0,0 +1,5 @@
+// Lo primero que queremos es que se muestren en consola todas las tareas que tiene pendientes el usuario. O sea, que aún no están terminadas. Ordenarlas por prioridad (Alta, Media, Baja).
+
+namespace mostrarTareas {
+    
+}
