4f28c77d824917457de68424a134dafbfe59ce42
jimezapolski <jimezapolski@gmail.com>
2024-04-03 12:29:50 -0300

    actualizacion

--- mostrarTareas.js
+++ mostrarTareas.js
@@ -1,1 +1,26 @@
-"use strict";
+// Lo primero que queremos es que se muestren en consola todas las tareas que tiene pendientes el usuario. O sea, que aún no están terminadas. Ordenarlas por prioridad (Alta, Media, Baja).
+var mostrarTareas;
+(function (mostrarTareas_1) {
+    var tareas = [
+        { nombreTarea: "Ordenar pieza", prioridad: "Media" },
+        { nombreTarea: "Lavar ropa", prioridad: "Alta" },
+        { nombreTarea: "Preparar la cena", prioridad: "Alta" },
+    ];
+    function mostrarTareas(tareas) {
+        for (var i = 0; i < tareas.length; i++) {
+            var tarea = tareas[i];
+            switch (tarea.prioridad) {
+                case "Alta": {
+                    return "".concat(tarea.nombreTarea, " es una tarea con prioridad Alta");
+                }
+                case "Media": {
+                    return "".concat(tarea.nombreTarea, " es una tarea con prioridad Media");
+                }
+                case "Baja": {
+                    return "".concat(tarea.nombreTarea, " es una tarea con prioridad Baja");
+                }
+            }
+        }
+        return "No hay tareas pendientes";
+    }
+})(mostrarTareas || (mostrarTareas = {}));
--- mostrarTareas.ts
+++ mostrarTareas.ts
@@ -1,5 +1,57 @@
 // Lo primero que queremos es que se muestren en consola todas las tareas que tiene pendientes el usuario. O sea, que aún no están terminadas. Ordenarlas por prioridad (Alta, Media, Baja).
-
 namespace mostrarTareas {
-    
+
+    interface Tareas {
+        nombreTarea: string,
+        prioridad: string,
+    }
+
+    const tareas: Tareas[] = [
+        {nombreTarea: "Ordenar pieza", prioridad:"Media"},
+        {nombreTarea: "Lavar ropa", prioridad:"Alta"},
+        {nombreTarea: "Preparar la cena", prioridad:"Alta"},
+    ];
+
+    type ResultadoTareas = 
+     | {tipo: "lista", tareas:Tareas}
+     | {tipo: "no hay tareas"}
+
+    function mostrarTareas (tareas:Tareas[]): string{
+        for( let i = 0; i < tareas.length; i++){
+            const tarea = tareas[i];
+            
+            switch (tarea.prioridad
+                ){
+                case "Alta": {
+                    return `${tarea.nombreTarea} es una tarea con prioridad Alta`
+                }
+                case "Media": {
+                    return `${tarea.nombreTarea} es una tarea con prioridad Media`
+                }
+                case "Baja": {
+                    return `${tarea.nombreTarea} es una tarea con prioridad Baja`
+                }
+              
+            }
+        }
+
+        return `No hay tareas pendientes`;
+    }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
